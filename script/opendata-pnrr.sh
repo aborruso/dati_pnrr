fdeed0c5889f2bd26ad08ead0345bd1caa4ad3c5
aborruso <aborruso@gmail.com>
2023-01-15 12:45:54 +0100

    update

--- script/opendata-pnrr.sh
+++ script/opendata-pnrr.sh
@@ -9,17 +9,23 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 URLbase="https://www.italiadomani.gov.it/content/sogei-ng/it/it/catalogo-open-data/jcr:content/root/container/opendatasearch.searchResults.html?orderby=%40jcr%3Acontent%2FobservationDateInEvidence&sort=desc&resultsOffset="
 
-for i in {0..16..8}; do
+# sfoglia i risultati di 8 in 8 (al momento sono meno di 24)
+for i in {0..80..8}; do
   echo "$i"
+  # donwload della pagina
   curl -kL "$URLbase$i" >"$folder/tmp.html"
-  #if grep <"$folder/tmp.html" 'Non sono presenti risultati'; then
-  #  echo "NON SONO PRESENTI RISULTATI"
-  #  break
-  #fi
-  grep <"$folder/tmp.html" -oP '/content/dam/sogei-ng/open-data/.*.csv' || true | while read -r path; do
+  # se non ci sono risultati esci dal ciclo
+  if grep <"$folder/tmp.html" 'Non sono presenti risultati'; then
+    echo "NON SONO PRESENTI RISULTATI"
+    break
+  fi
+  # estrai i link ai csv
+  cat "$folder/tmp.html" | grep -oP '/content/dam/sogei-ng/open-data/.+?.csv' | while read -r path; do
     url="https://www.italiadomani.gov.it$path"
-    echo $url
-    curl -kL "$url" -o "$folder/../data/$(basename "$url")"
+
+    filename=$(basename "$path" .csv)
+    # scarica i csv
+  curl -kL "$url" -o "$folder/../data/$filename.csv"
   done
 done
 
