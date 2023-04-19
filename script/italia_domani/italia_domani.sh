f71a50f84af4545c7144e3f3f2819081738394c2
aborruso <aborruso@gmail.com>
2023-04-20 00:39:33 +0200

    dati italia domani

--- script/italia_domani/italia_domani.sh
+++ script/italia_domani/italia_domani.sh
@@ -15,7 +15,7 @@ URLbase="https://www.italiadomani.gov.it/content/sogei-ng/it/it/catalogo-open-da
 for i in {0..80..8}; do
   echo "$i"
   # donwload della pagina
-  curl -kL "$URLbase$i" >"$folder/tmp.html"
+  curl -kL "$URLbase$i" -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' --compressed>"$folder/tmp.html"
   # se non ci sono risultati esci dal ciclo
   if grep <"$folder/tmp.html" 'Non sono presenti risultati'; then
     echo "Non ci sono più pagine con dati"
@@ -23,12 +23,18 @@ for i in {0..80..8}; do
   fi
   # estrai i link ai csv
   cat "$folder/tmp.html" | grep -oP '/content/dam/sogei-ng/open-data/.+?.csv' | while read -r path; do
-    url="https://www.italiadomani.gov.it$path"
+    url=$(echo "https://www.italiadomani.gov.it$path" | sed 's/ /%20/g')
 
     filename=$(basename "$path" .csv)
     # scarica i csv
-    curl -kL "$url" -o "$folder/../../data/$nome/$filename.csv"
+    curl -kL "$url" -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' --compressed -o "$folder/../../data/$nome/$filename.csv"
   done
 done
 
 echo "sono fuori dal ciclo"
+
+find "$folder"/../../data/"$nome" -type f -size +100M -print0 | while IFS= read -r -d '' file;do
+  basename=$(basename "$file" .csv)
+  gzip -c "$file" > "$folder"/../../data/"$nome"/"$basename".csv.gz
+  rm -f "$file"
+done
